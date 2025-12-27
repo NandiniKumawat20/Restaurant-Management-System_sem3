33630feed1ec4e7f29ba388dd94618d390a92e5e
NandiniKumawat20 <nandini20.kumawat@gmail.com>
2025-12-27 08:19:30 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const { body, validationResult } = require('express-validator');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5500; // Changed to 5500 as per your setup
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
